6cb1a609ae3dd1fcde66f49eb0db4434fcebb2ee
Sahej Bansal <sahejbansal2002@gmail.com>
2022-09-08 12:32:11 -0400

    Updates

---
+++ precache-manifest.4a8951f26cc316f5815bf6cde4b6d615.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ed7803f1022df20e59a3621f6f1619c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "feef6dd2d4d6196c85b6",
+    "url": "/static/css/2.ad5ce37f.chunk.css"
+  },
+  {
+    "revision": "22e20e9dec474086884a",
+    "url": "/static/css/main.b7805646.chunk.css"
+  },
+  {
+    "revision": "feef6dd2d4d6196c85b6",
+    "url": "/static/js/2.b9d90157.chunk.js"
+  },
+  {
+    "revision": "b12aaea9c7d723083f2c844fa56b8dc2",
+    "url": "/static/js/2.b9d90157.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22e20e9dec474086884a",
+    "url": "/static/js/main.3da39878.chunk.js"
+  },
+  {
+    "revision": "0c57f973157196477324",
+    "url": "/static/js/runtime-main.6b5ef6cf.js"
+  }
+]);
